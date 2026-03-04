2075c1fd7dfc8b0762e6a0db761772a0ca43bf53
KelverBraga <kelverruanbraga@gmail.com>
2026-03-03 23:17:48 -0300

    Make logout

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
 
   allow_browser versions: :modern
 
+  before_action :authenticate_user!
+
   stale_when_importmap_changes
 
 end
--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-  
+    @test = "Testando o controller"
   end
 
 end
